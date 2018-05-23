a9cd5868faa5b4af741e7cd99ce8d3ce4633ef16
guoxingx <3064793912@qq.com>
2018-05-23 21:00:19 +0800

    pow

--- block.go
+++ block.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-    "bytes"
-    "crypto/sha256"
-    "strconv"
     "time"
 )
 
@@ -13,20 +10,17 @@ type Block struct {
 	Data		  []byte
 	PrevBlockHash []byte
 	Hash		  []byte
-}
-
-func (b *Block) SetHash() {
-	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
-	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
-	hash := sha256.Sum256(headers)
-
-	b.Hash = hash[:]
+    None          int
 }
 
 func NewBlock(data string, prevBlockHash []byte) *Block {
-	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
-	block.SetHash()
-	return block
+    block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
+    pow := NewProofOfWork(block)
+
+    nonce, hash := pow.Run()
+    block.Hash = hash[:]
+    block.None = nonce
+    return block
 }
 
 func NewGenesisBlock() *Block {
--- proofofwork.go
+++ proofofwork.go
@@ -1,10 +1,14 @@
 package main
 
 import (
-
+    "math"
+    "math/big"
+    "bytes"
+    "crypto/sha256"
+    "fmt"
 )
 
-const targetBits = 24
+const targetBits = 22
 
 type ProofOfWork struct {
     block *Block
@@ -33,4 +37,27 @@ func (pow *ProofOfWork) prepareData(nonce int) []byte {
     )
 
     return data
+}
+
+func (pow *ProofOfWork) Run() (int, []byte) {
+    var hashInt big.Int
+    var hash [32]byte
+    nonce := 0
+
+    fmt.Printf("Mining the block containing \"%s\"\n", pow.block.Data)
+    for nonce < math.MaxInt64 {
+        data := pow.prepareData(nonce)
+        hash = sha256.Sum256(data)
+        hashInt.SetBytes(hash[:])
+
+        if hashInt.Cmp(pow.target) == -1 {
+            fmt.Printf("\r%x", hash)
+            break
+        } else {
+            nonce ++
+        }
+    }
+    fmt.Printf("\n\n")
+
+    return nonce, hash[:]
 }
